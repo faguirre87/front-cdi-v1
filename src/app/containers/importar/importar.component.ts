70496e80f8443fbc0b301375f12fe7e0c0828bc1
faguirre87 <fede_aguirre_utn@hotmail.com>
2019-05-15 17:10:46 -0300

    first commit

--- src/app/containers/importar/importar.component.ts
+++ src/app/containers/importar/importar.component.ts
@@ -1,18 +1,102 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-importar',
   templateUrl: './importar.component.html',
   styleUrls: ['./importar.component.scss']
 })
-export class ImportarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-
+export class ImportarComponent {
 
  
+  public csvRecords: any[] = [];
+
+  @ViewChild('fileImportInput') fileImportInput: any;
+
+  fileChangeListener($event: any): void {
+
+    let text = [];
+    let files = $event.srcElement.files;
+
+    if (this.isCSVFile(files[0])) {
+
+      let input = $event.target;
+      let reader = new FileReader();
+      reader.readAsText(input.files[0]);
+
+      reader.onload = () => {
+        let csvData = reader.result;
+        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
+
+        let headersRow = this.getHeaderArray(csvRecordsArray);
+
+        this.csvRecords = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+      };
+
+      reader.onerror = function () {
+        alert('Unable to read ' + input.files[0]);
+      };
+
+    } else {
+      alert("Please import valid .csv file.");
+      this.fileReset();
+    }
+  }
+
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {
+    let dataArr = [];
+
+    for (let i = 1; i < csvRecordsArray.length; i++) {
+      let data = (<string>csvRecordsArray[i]).split(',');
+
+      // FOR EACH ROW IN CSV FILE IF THE NUMBER OF COLUMNS
+      // ARE SAME AS NUMBER OF HEADER COLUMNS THEN PARSE THE DATA
+      if (data.length == headerLength) {
+
+        let csvRecord: CSVRecord = new CSVRecord();
+
+        csvRecord.NumOrden = data[0].trim();
+        csvRecord.email = data[1].trim();
+        csvRecord.nombreNiño = data[2].trim();
+        
+
+        dataArr.push(csvRecord);
+      }
+    }
+    return dataArr;
+  }
+
+  // CHECK IF FILE IS A VALID CSV FILE
+  isCSVFile(file: any) {
+    return file.name.endsWith(".csv");
+  }
+
+  // GET CSV FILE HEADER COLUMNS
+  getHeaderArray(csvRecordsArr: any) {
+    let headers = (<string>csvRecordsArr[0]).split(',');
+    let headerArray = [];
+    for (let j = 0; j < headers.length; j++) {
+      headerArray.push(headers[j]);
+    }
+    return headerArray;
+  }
+
+  fileReset() {
+    this.fileImportInput.nativeElement.value = "";
+    this.csvRecords = [];
+  }
+
+}
+
+export class CSVRecord {
+
+  
+  public nombreNiño: string;
+  public NumOrden: string;
+  public email: string;
+
+
+  constructor() {
+
+  }
 }
